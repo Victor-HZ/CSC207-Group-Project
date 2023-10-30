4b1c7dbde5c5463ccd1af65c8e75df5d7668cfc4
minjaesong04 <minjae.song@mail.utoronto.ca>
2023-10-30 12:31:06 -0400

    updated date to include necessary information

--- src/dayinfo/Date.java
+++ src/dayinfo/Date.java
@@ -1,8 +1,24 @@
 package dayinfo;
 
 public class Date implements DayInfo {
-    // TODO
-    @Override
-    public String getInfo() {return null;}
+    public String start;
+    public String end;
+    public Integer offset;
 
+    public void updateStart(String day) {
+        // Day should be in the format of year-month-day T start hour (2018-04-02T00:00:00)
+        start = day;
+    }
+
+    public void updateEnd(String day) {
+        // Day should be in the format of year-month-day T start hour (2018-04-02T00:00:00)
+        end = day;
+    }
+
+    public Integer getOffset() {
+        Integer startHour = Integer.parseInt(start.substring(10, 13));
+        Integer endHour = Integer.parseInt(end.substring(10, 13));
+        offset = endHour - startHour;
+        return offset;
+    }
 }
