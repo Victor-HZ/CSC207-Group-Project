2cfb112a010535a8bd80213e3fd7b3d16602817a
minjaesong04 <minjae.song@mail.utoronto.ca>
2023-11-13 11:12:05 -0500

    Fixed bugs in weather

--- src/plan/entity/day_info/Weather.java
+++ src/plan/entity/day_info/Weather.java
@@ -1,24 +1,17 @@
 package plan.entity.day_info;
 import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public class Weather implements DayInfo {
+public class Weather {
 
     public String rain;
     public String weather;
 
     // Must be within 14 days
     public void updateWeather(Date dayInfo, Coordinate coordinate) throws IOException {
-        String coordinates = coordinate.getCoor();
-        int divider = coordinates.indexOf(',');
-        String longitude = coordinates.substring(0, divider);
-        String latitude = coordinates.substring(divider + 1);
-        String startDate = dayInfo.getStart();
-        String endDate = dayInfo.getEnd();
-
-        String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude +
-                "&hourly=temperature_2m,rain&start_date=" + startDate + "&end_date=" + endDate;
+        String apiUrl = getString(dayInfo, coordinate);
 
 
         OkHttpClient client = new OkHttpClient().newBuilder()
@@ -27,11 +20,25 @@ public class Weather implements DayInfo {
                 .url(apiUrl)
                 .build();
         try (Response response = client.newCall(request).execute()) {
+            assert response.body() != null;
             weather = response.body().string();
             // Try and make weather more readable
             // Add something to change weather
         }
 
+    }
+
+    @NotNull
+    private static String getString(Date dayInfo, Coordinate coordinate) {
+        String coordinates = coordinate.getCoor();
+        int divider = coordinates.indexOf(',');
+        String longitude = coordinates.substring(0, divider);
+        String latitude = coordinates.substring(divider + 1);
+        String startDate = dayInfo.getStart();
+        String endDate = dayInfo.getEnd();
+
+        return "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,rain&start_date=" + startDate + "&end_date=" + endDate;
     }
 }
 
