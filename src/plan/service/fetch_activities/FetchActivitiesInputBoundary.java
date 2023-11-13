2f9c86b5287a3e2d2d4ae6f5894572c920712558
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-12 23:21:09 -0500

    implement fetch activities input boundary and controller

--- src/plan/service/fetch_activities/FetchActivitiesInputBoundary.java
+++ src/plan/service/fetch_activities/FetchActivitiesInputBoundary.java
@@ -1,4 +1,8 @@
 package plan.service.fetch_activities;
 
+import plan.entity.address.Address;
+import plan.entity.day_info.DayInfo;
+
 public interface FetchActivitiesInputBoundary {
+    void execute(DayInfo date, Address addresss);
 }
--- src/plan/service/fetch_activities/interface_adapter/FetchActivitiesController.java
+++ src/plan/service/fetch_activities/interface_adapter/FetchActivitiesController.java
@@ -1,4 +1,17 @@
 package plan.service.fetch_activities.interface_adapter;
 
+import plan.entity.address.Address;
+import plan.entity.day_info.DayInfo;
+import plan.service.fetch_activities.FetchActivitiesInputBoundary;
+
 public class FetchActivitiesController {
+    final FetchActivitiesInputBoundary fetchActivitiesInteractor;
+
+    public FetchActivitiesController(FetchActivitiesInputBoundary fetchActivitiesInteractor){
+        this.fetchActivitiesInteractor = fetchActivitiesInteractor;
+    }
+
+    public void execute(DayInfo date, Address address){
+        fetchActivitiesInteractor.execute(date, address);
+    }
 }
