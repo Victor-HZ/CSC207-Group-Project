4157062eb14a20958544d2c1a8df8c950c6974d1
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-13 12:47:24 -0500

    implemented Plan interface.

--- src/plan/entity/plan/Plan.java
+++ src/plan/entity/plan/Plan.java
@@ -1,7 +1,14 @@
 package plan.entity.plan;
 
 import plan.entity.activity.Activity;
+import plan.entity.day_info.DayInfo;
+
+import java.util.ArrayList;
 
 public interface Plan {
-
+    void addActivity(Activity activity);
+    boolean deleteActivity(Activity activity);
+    double getCost();
+    ArrayList<Activity> getActivities();
+    DayInfo getDayInfo();
 }
