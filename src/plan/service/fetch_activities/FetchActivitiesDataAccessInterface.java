fdf85bd7adfc39f23f4b33ead9f4cc5fd6a8b4f0
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-12 23:34:23 -0500

    implement state, input date for fetch data

--- src/plan/service/fetch_activities/FetchActivitiesDataAccessInterface.java
+++ src/plan/service/fetch_activities/FetchActivitiesDataAccessInterface.java
@@ -1,4 +1,7 @@
 package plan.service.fetch_activities;
 
+import plan.service.fetch_activities.interface_adapter.FetchActivitiesController;
+
 public interface FetchActivitiesDataAccessInterface {
+
 }
--- src/plan/service/fetch_activities/FetchActivitiesInputData.java
+++ src/plan/service/fetch_activities/FetchActivitiesInputData.java
@@ -1,4 +1,22 @@
 package plan.service.fetch_activities;
 
+import plan.entity.address.Address;
+import plan.entity.day_info.DayInfo;
+
 public class FetchActivitiesInputData {
+    private final Address address;
+    private final DayInfo date;
+
+    public FetchActivitiesInputData(Address address, DayInfo date){
+        this.address = address;
+        this.date = date;
+    }
+
+    Address getAddress(){
+        return address;
+    }
+
+    DayInfo getDate(){
+        return date;
+    }
 }
