0ef162436aac855d5d32bcbf7f2dbd70b308bf80
J Sun <jishna.sunkara@gmail.com>
2023-11-13 12:52:09 -0500

    Updated AddActivity OutputData, OutputBoundary, and Presenter

--- src/plan/service/add_activity/AddActivityOutputData.java
+++ src/plan/service/add_activity/AddActivityOutputData.java
@@ -1,4 +1,9 @@
 package plan.service.add_activity;
 
 public class AddActivityOutputData {
+    private boolean useCaseFailed;
+
+    public AddActivityOutputData(boolean useCaseFailed) {
+        this.useCaseFailed = useCaseFailed;
+    }
 }
