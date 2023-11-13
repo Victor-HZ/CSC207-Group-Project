ee777535e8c26ad9a96de3fe9fce1f69806ffea1
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-12 23:27:26 -0500

    implement output boundary and data for fetch activities

--- src/plan/service/fetch_activities/FetchActivitiesOutputData.java
+++ src/plan/service/fetch_activities/FetchActivitiesOutputData.java
@@ -1,4 +1,19 @@
 package plan.service.fetch_activities;
 
+import plan.entity.activity.Activity;
+
+import java.util.ArrayList;
+
 public class FetchActivitiesOutputData {
+    private final ArrayList<Activity> activities;
+    private final boolean useCaseFaild;
+
+    public FetchActivitiesOutputData(ArrayList<Activity> activities, boolean useCaseFaild){
+        this.activities = activities;
+        this.useCaseFaild = useCaseFaild;
+    }
+
+    public ArrayList<Activity> getActivities(){
+        return this.activities;
+    }
 }
