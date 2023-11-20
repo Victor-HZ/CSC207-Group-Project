1d0cefebf103aa1346549fd79b3d887a72f6008d
starw <jishna.sunkara@mail.utoronto.ca>
2023-11-20 12:48:12 -0500

    Extended ViewModel class in FetchActivites ViewModel and CreatePlan ViewModel

--- src/plan/service/create_plan/interface_adapter/CreatePlanViewModel.java
+++ src/plan/service/create_plan/interface_adapter/CreatePlanViewModel.java
@@ -1,4 +1,21 @@
 package plan.service.create_plan.interface_adapter;
 
-public class CreatePlanViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class CreatePlanViewModel extends ViewModel {
+    public CreatePlanViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
--- src/plan/service/fetch_activities/interface_adapter/FetchActivitiesViewModel.java
+++ src/plan/service/fetch_activities/interface_adapter/FetchActivitiesViewModel.java
@@ -1,4 +1,21 @@
 package plan.service.fetch_activities.interface_adapter;
 
-public class FetchActivitiesViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class FetchActivitiesViewModel extends ViewModel {
+    public FetchActivitiesViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
