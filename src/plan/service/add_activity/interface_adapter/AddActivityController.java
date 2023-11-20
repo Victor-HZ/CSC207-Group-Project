1d815d4e4ffa5432794b59e8b5b9b85ca2a1d830
J Sun <jishna.sunkara@gmail.com>
2023-11-20 11:51:34 -0500

    Updated AddActivity Interactor and AddActivity Controller

--- src/plan/service/add_activity/interface_adapter/AddActivityController.java
+++ src/plan/service/add_activity/interface_adapter/AddActivityController.java
@@ -1,4 +1,19 @@
 package plan.service.add_activity.interface_adapter;
 
+import plan.entity.activity.Activity;
+import plan.entity.plan.Plan;
+import plan.service.add_activity.AddActivityInputBoundary;
+import plan.service.add_activity.AddActivityInputData;
+
 public class AddActivityController {
+    final AddActivityInputBoundary addActivityInteractor;
+    public AddActivityController(AddActivityInputBoundary addActivityInteractor) {
+        this.addActivityInteractor = addActivityInteractor;
+    }
+
+    public void execute(Plan plan, Activity activity) {
+        AddActivityInputData addActivityInputData = new AddActivityInputData(plan, activity);
+
+        addActivityInteractor.execute(addActivityInputData);
+    }
 }
