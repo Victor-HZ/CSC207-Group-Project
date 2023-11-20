7d0c2152fd195a367a5abfe4d805025bf78a9b8a
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-20 14:26:25 -0500

    Include TripAdvisorAPI.

---
+++ src/apis/tripAdvisor/TripAdvisorAPI.java
@@ -0,0 +1,68 @@
+package apis.tripAdvisor;
+
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.util.ArrayList;
+
+import apis.ActivitiesFetchInterface;
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import plan.entity.activity.Activity;
+import plan.entity.activity.Event;
+import plan.entity.address.Address;
+import plan.entity.address.CanadaAddress;
+import plan.entity.day_info.Date;
+import plan.entity.day_info.DayInfo;
+import plan.entity.day_info.ToStringType;
+
+
+public class TripAdvisorAPI implements ActivitiesFetchInterface {
+
+    private static final String API_TOKEN = System.getenv("TRIPADVISOR_API_TOKEN"); // "6F44BDF370124DB2BF884B9B21666F43"
+
+
+    @Override
+    public ArrayList<Activity> getEvents(String city, DayInfo date) throws JSONException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        HttpUrl.Builder httpBuilder = HttpUrl.parse("https://app.ticketmaster.com/discovery/v2/events").newBuilder()
+                .addQueryParameter("apikey", API_TOKEN)
+                .addQueryParameter("startDateTime", date.toString(ToStringType.TICKETMASTER))
+                .addQueryParameter("countryCode", "CA");
+        Request request = new Request.Builder().url(httpBuilder.build()).build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+
+            if (response.code() == 200) {
+                assert response.body() != null;
+                JSONObject responseBody = new JSONObject(response.body().string());
+                JSONObject result = responseBody.getJSONObject("_embedded");
+
+                JSONArray membersArray = result.getJSONArray("events");
+                ArrayList<Activity> activities = new ArrayList<>();
+                for (int i = 0; i < membersArray.length(); i++) {
+                    Activity event = new Event();
+                    Address address = new CanadaAddress();
+                    try {
+                        event.setName(membersArray.getJSONObject(i).getString("name"));
+                        event.setCost(membersArray.getJSONObject(1).getJSONArray("priceRanges").getJSONObject(0).getDouble("min"));
+                        event.setAddress(address);
+                        event.setDescription(membersArray.getJSONObject(i).getString("info"));
+                        activities.add(event);
+                    } catch (Exception ignored) {
+
+                    }
+                }
+                return activities;
+            } else {
+                assert response.body() != null;
+                JSONObject responseBody = new JSONObject(response.body().string());
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
