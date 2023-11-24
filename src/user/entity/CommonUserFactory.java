d54344eaacfa3d6aa03d209d7ac979da33bd4078
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-24 11:39:55 -0500

    implements the api token into user system

--- src/user/entity/CommonUserFactory.java
+++ src/user/entity/CommonUserFactory.java
@@ -1,6 +1,7 @@
 package user.entity;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 public class CommonUserFactory implements UserFactory {
     /**
@@ -11,7 +12,7 @@ public class CommonUserFactory implements UserFactory {
      */
 
     @Override
-    public User create(String name, String password, LocalDateTime ltd) {
-        return new CommonUser(name, password, ltd);
+    public User create(String name, String password, LocalDateTime ltd, HashMap<User.API_TOKEN, String> apiTokens) {
+        return new CommonUser(name, password, ltd, apiTokens);
     }
 }
--- src/user/entity/UserFactory.java
+++ src/user/entity/UserFactory.java
@@ -1,8 +1,9 @@
 package user.entity;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 public interface UserFactory {
     /** Requires: password is valid. */
-    User create(String name, String password, LocalDateTime ltd);
+    User create(String name, String password, LocalDateTime ltd, HashMap<User.API_TOKEN, String> apiTokens);
 }
