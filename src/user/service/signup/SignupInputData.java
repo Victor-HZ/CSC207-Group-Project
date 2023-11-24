8edee48d8a247946a76a725ecdd7a8c06d1c8515
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-11-24 11:46:34 -0500

    implements the api token into user sign up

--- src/user/service/signup/SignupInputData.java
+++ src/user/service/signup/SignupInputData.java
@@ -1,15 +1,21 @@
 package user.service.signup;
 
+import user.entity.User;
+
+import java.util.HashMap;
+
 public class SignupInputData {
 
     final private String username;
     final private String password;
     final private String repeatPassword;
+    final private HashMap<User.API_TOKEN, String> apiTokens;
 
-    public SignupInputData(String username, String password, String repeatPassword) {
+    public SignupInputData(String username, String password, String repeatPassword, HashMap<User.API_TOKEN, String> apiTokens) {
         this.username = username;
         this.password = password;
         this.repeatPassword = repeatPassword;
+        this.apiTokens = apiTokens;
     }
 
     String getUsername() {
@@ -22,5 +28,9 @@ public class SignupInputData {
 
     public String getRepeatPassword() {
         return repeatPassword;
+    }
+
+    public HashMap<User.API_TOKEN, String> getAPIToken(){
+        return apiTokens;
     }
 }
