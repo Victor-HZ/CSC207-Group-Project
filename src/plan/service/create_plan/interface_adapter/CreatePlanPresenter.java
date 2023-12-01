6a08d16cc68cde3be5de9c0f3d44871a85149fe2
minjaesong04 <minjae.song@mail.utoronto.ca>
2023-12-01 10:58:45 -0500

    Coded the create plan use case and changed the logged in use state/use case to match

--- src/plan/service/create_plan/interface_adapter/CreatePlanPresenter.java
+++ src/plan/service/create_plan/interface_adapter/CreatePlanPresenter.java
@@ -1,6 +1,48 @@
 package plan.service.create_plan.interface_adapter;
 
 import plan.service.create_plan.CreatePlanOutputBoundary;
+import plan.service.create_plan.CreatePlanOutputData;
+import plan.service.main_view_models.EditorState;
+import plan.service.main_view_models.EditorViewModel;
+import plan.service.main_view_models.StartUpState;
+import plan.service.main_view_models.StartUpViewModel;
+import user.entity.User;
+import user.service.logged_in.interface_adaper.LoggedInState;
+import user.service.logged_in.interface_adaper.LoggedInViewModel;
+import user.service.signup.SignupOutputBoundary;
+import user.service.signup.SignupOutputData;
+import view.LoggedInView;
+import view.ViewManager;
+import view.interface_adapter.ViewManagerModel;
 
 public class CreatePlanPresenter implements CreatePlanOutputBoundary {
+    private final LoggedInViewModel loggedInViewModel;
+    private final EditorViewModel editorViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public CreatePlanPresenter(LoggedInViewModel loggedInViewModel,
+                               EditorViewModel editorViewModel,
+                               ViewManagerModel viewManagerModel) {
+        this.loggedInViewModel = loggedInViewModel;
+        this.editorViewModel = editorViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareEditorView(CreatePlanOutputData user) {
+        // Switch to editor view from start up view
+        LoggedInState startUpState = loggedInViewModel.getState();
+        this.loggedInViewModel.setState(startUpState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(editorViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setCreatePlanError(error);
+        loggedInViewModel.firePropertyChanged();
+    }
 }
