5c517dccf76b8e3f2fde9874cd1668eb158c56b9
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-03 16:12:52 -0500

    fix omitted ';' from import statement

--- src/plan/service/load_plan/LoadPlanInputBoundary.java
+++ src/plan/service/load_plan/LoadPlanInputBoundary.java
@@ -1,5 +1,5 @@
 package plan.service.load_plan;
-import plan.entity.plan.Plan
+import plan.entity.plan.Plan;
 
 public interface LoadPlanInputBoundary {
     void loadPlan(LoadPlanInputData inputData);
