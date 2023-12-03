2f41179d5518b314fda44c621fa639cbd8ac3d2e
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-02 21:00:36 -0500

    partially finished EditorUseCaseFactory.java

--- src/app/EditorUseCaseFactory.java
+++ src/app/EditorUseCaseFactory.java
@@ -1,30 +1,30 @@
 package app;
 
 import apis.ActivitiesFetchInterface;
-import plan.service.add_activity.AddActivityDataAccessInterface;
 import plan.service.add_activity.AddActivityInputBoundary;
 import plan.service.add_activity.AddActivityInteractor;
 import plan.service.add_activity.AddActivityOutputBoundary;
 import plan.service.add_activity.interface_adapter.AddActivityController;
 import plan.service.add_activity.interface_adapter.AddActivityPresenter;
-import plan.service.delete_activity.DeleteActivityDataAccessInterface;
 import plan.service.delete_activity.DeleteActivityInputBoundary;
 import plan.service.delete_activity.DeleteActivityInteractor;
 import plan.service.delete_activity.DeleteActivityOutputBoundary;
 import plan.service.delete_activity.interface_adapter.DeleteActivityController;
 import plan.service.delete_activity.interface_adapter.DeleteActivityPresenter;
-import plan.service.fetch_activities.FetchActivitiesDataAccessInterface;
 import plan.service.fetch_activities.FetchActivitiesInputBoundary;
 import plan.service.fetch_activities.FetchActivitiesInteractor;
 import plan.service.fetch_activities.FetchActivitiesOutputBoundary;
 import plan.service.fetch_activities.interface_adapter.FetchActivitiesController;
 import plan.service.fetch_activities.interface_adapter.FetchActivitiesPresenter;
 import plan.service.main_view_models.EditorViewModel;
+import plan.service.save_plan.SavePlanInputBoundary;
+import plan.service.save_plan.SavePlanInteractor;
+import plan.service.save_plan.SavePlanOutputBoundary;
+import plan.service.save_plan.interface_adapter.SavePlanController;
+import plan.service.save_plan.interface_adapter.SavePlanPresenter;
 import view.EditorView;
 import view.interface_adapter.ViewManagerModel;
-import view.interface_adapter.ViewModel;
 
-import javax.swing.text.View;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -35,12 +35,9 @@ public class EditorUseCaseFactory {
     public static EditorView create(
             ViewManagerModel viewManagerModel,
             EditorViewModel editorViewModel,
-            FetchActivitiesDataAccessInterface fetchActivitiesDataAccessInterface,
-            DeleteActivityDataAccessInterface deleteActivityDataAccessInterface,
-            AddActivityDataAccessInterface addActivityDataAccessInterface,
             ArrayList<ActivitiesFetchInterface> activitiesFetchInterfaces){
         try {
-            FetchActivitiesController fetchActivitiesController = createFetchActivitiesUseCase(fetchActivitiesDataAccessInterface, activitiesFetchInterfaces);
+            FetchActivitiesController fetchActivitiesController = createFetchActivitiesUseCase(activitiesFetchInterfaces);
             AddActivityController addActivityController = createAddActivityController(viewManagerModel, editorViewModel);
             DeleteActivityController deleteActivityController = createDeleteActivityController();
             return new EditorView(editorViewModel, addActivityController, deleteActivityController, fetchActivitiesController);
@@ -49,7 +46,7 @@ public class EditorUseCaseFactory {
         }
     }
 
-    private static AddActivityController createAddActivityController(ViewManagerModel viewManagerModel, EditorViewModel editorScreen, AddActivityInputBoundary){
+    private static AddActivityController createAddActivityController(ViewManagerModel viewManagerModel, EditorViewModel editorScreen){
         AddActivityOutputBoundary addActivityOutputBoundary = new AddActivityPresenter(viewManagerModel, editorScreen);
         AddActivityInputBoundary addActivityInteractor = new AddActivityInteractor(addActivityOutputBoundary);
         return new AddActivityController(addActivityInteractor);
@@ -62,9 +59,15 @@ public class EditorUseCaseFactory {
 
     }
 
-    private static FetchActivitiesController createFetchActivitiesUseCase(FetchActivitiesDataAccessInterface fetchActivitiesDataAccessInterface, ArrayList<ActivitiesFetchInterface> activitiesFetchInterfaces) throws IOException {
+    private static FetchActivitiesController createFetchActivitiesUseCase(ArrayList<ActivitiesFetchInterface> activitiesFetchInterfaces) throws IOException {
         FetchActivitiesOutputBoundary fetchActivitiesOutputBoundary = new FetchActivitiesPresenter();
-        FetchActivitiesInputBoundary fetchActivitiesInteractor = new FetchActivitiesInteractor(fetchActivitiesDataAccessInterface, fetchActivitiesOutputBoundary, activitiesFetchInterfaces);
+        FetchActivitiesInputBoundary fetchActivitiesInteractor = new FetchActivitiesInteractor(fetchActivitiesOutputBoundary, activitiesFetchInterfaces);
         return new FetchActivitiesController(fetchActivitiesInteractor);
+    }
+
+    private static SavePlanController createSavePlanUseCase(){
+        SavePlanOutputBoundary savePlanOutputBoundary = new SavePlanPresenter();
+        SavePlanInputBoundary savePlanInteractor = new SavePlanInteractor();
+        return new SavePlanController();
     }
 }
