babec50d7a42d54fd91e6bd75220a7ae8948d811
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-03 17:45:03 -0500

    include a popup for failed log in

--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -167,10 +167,6 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                 }
         );
 
-        // This makes a new KeyListener implementing class, instantiates it, and
-        // makes it listen to keystrokes in the usernameInputField.
-        //
-        // Notice how it has access to instance variables in the enclosing class!
         usernameInputField.addKeyListener(
                 new KeyListener() {
                     @Override
