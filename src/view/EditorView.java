63df51274a73be7812e5a6d7af79c02a210afde3
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-03 20:40:08 -0500

    continue working on the view

--- src/view/EditorView.java
+++ src/view/EditorView.java
@@ -1,16 +1,18 @@
 package view;
 
-import apis.ActivitiesFetchInterface;
 import plan.entity.activity.Activity;
+import plan.entity.address.Address;
 import plan.entity.address.CanadaAddress;
 import plan.entity.day_info.Date;
-import plan.entity.day_info.ToStringType;
+import plan.entity.plan.Plan;
 import plan.service.add_activity.interface_adapter.AddActivityController;
 import plan.service.delete_activity.interface_adapter.DeleteActivityController;
 import plan.service.fetch_activities.interface_adapter.FetchActivitiesController;
+import plan.service.generate_report.interface_adapter.GenerateReportController;
 import plan.service.main_view_models.EditorState;
 import plan.service.main_view_models.EditorViewModel;
-import user.service.signup.interface_adapter.SignupState;
+import plan.service.save_plan.interface_adapter.SavePlanController;
+import user.entity.User;
 
 import javax.swing.*;
 import javax.swing.event.AncestorEvent;
@@ -24,7 +26,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EditorView extends JPanel implements ActionListener, PropertyChangeListener {
-    public final String viewName = "editor";
+    public final String viewName = "plan editor";
 
     private final EditorViewModel editorViewModel;
     private final JTable availableActivities;
@@ -33,35 +35,50 @@ public class EditorView extends JPanel implements ActionListener, PropertyChange
     private final AddActivityController addActivityController;
     private final DeleteActivityController deleteActivityController;
     private final FetchActivitiesController fetchActivitiesController;
+    private final GenerateReportController generateReportController;
+    private final SavePlanController savePlanController;
 
     private final JButton addActivity;
     private final JButton deleteActivity;
     private final JButton savePlan;
     private final JButton generateReport;
 
-    public EditorView(EditorViewModel editorViewModel, AddActivityController addActivityController, DeleteActivityController deleteActivityController, FetchActivitiesController fetchActivitiesController){
+    public EditorView(EditorViewModel editorViewModel,
+                      AddActivityController addActivityController,
+                      DeleteActivityController deleteActivityController,
+                      FetchActivitiesController fetchActivitiesController,
+                      GenerateReportController generateReportController,
+                      SavePlanController savePlanController,
+                      Plan plan,
+                      Address address,
+                      User user){
         this.editorViewModel = editorViewModel;
         this.addActivityController = addActivityController;
         this.deleteActivityController = deleteActivityController;
         this.fetchActivitiesController = fetchActivitiesController;
+        this.generateReportController = generateReportController;
+        this.savePlanController = savePlanController;
+
         editorViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel(EditorViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-//        Temp helper code
-        Date date = new Date();
-        date.setYear(2023);
-        date.setMonth(12);
-        date.setDay(5);
-        date.setHour(12);
-        CanadaAddress address = new CanadaAddress();
-        address.setCity("Toronto");
-        address.setCountry("Canada");
-        address.setPostCode("M6K, 3C3");
-        address.setStreetNumber(170);
-        address.setStreetName("Princes' Blvd");
-        ArrayList<Activity> activities = this.fetchActivitiesController.execute(date, address);
+        EditorState state = editorViewModel.getState();
+        state.setPlan(plan);
+        state.setAddress(address);
+        state.setUser(user);
+
+
+        state.setApiTokens(User.API_TOKEN.Ticketmaster, state.getUser().getAPIToken(User.API_TOKEN.Ticketmaster));
+        state.setApiTokens(User.API_TOKEN.TripAdvisor, state.getUser().getAPIToken(User.API_TOKEN.TripAdvisor));
+        state.setApiTokens(User.API_TOKEN.Coordinate, state.getUser().getAPIToken(User.API_TOKEN.Coordinate));
+
+        editorViewModel.setState(state);
+
+        state = editorViewModel.getState();
+
+        ArrayList<Activity> activities = this.fetchActivitiesController.execute(state.getDayInfo(), state.getAddress(), state.getApiTokens());
         HashMap<Integer, Activity> hashActivities = new HashMap<>();
         Integer i = 0;
         for (Activity activity: activities){
@@ -79,7 +96,7 @@ public class EditorView extends JPanel implements ActionListener, PropertyChange
         String[] columnNames = {"Index", "Name", "Cost", "Address", "Time"};
         String[][] displayActivitiesArray = new String[displayActivities.size()][100];
         String[][] selectedActivitiesArray = new String[displayActivities.size()][100];
-        displayActivities.toArray(displayActivitiesArray);
+//        displayActivities.toArray(displayActivitiesArray);
         availableActivities = new JTable(displayActivitiesArray, columnNames);
         availableActivities.setCellSelectionEnabled(true);
         selectedActivities = new JTable(selectedActivitiesArray, columnNames);
@@ -105,6 +122,7 @@ public class EditorView extends JPanel implements ActionListener, PropertyChange
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(addActivity)) {
                             EditorState currentState = editorViewModel.getState();
+//                            addActivityController.execute()
                         }
                     }
                 }
