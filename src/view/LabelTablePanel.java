41b03820211da209b799f2dc3ebb5cfc5ce742d4
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-03 22:36:45 -0500

    First attempt to implement the editor view

--- src/view/LabelTablePanel.java
+++ src/view/LabelTablePanel.java
@@ -6,7 +6,7 @@ import javax.swing.*;
  * A panel containing a label and a text field.
  */
 class LabelTablePanel extends JPanel {
-    LabelTablePanel(JLabel label, JTable table) {
+    LabelTablePanel(JLabel label, JScrollPane table) {
         this.add(label);
         this.add(table);
     }
