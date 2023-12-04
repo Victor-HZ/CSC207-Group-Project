6127bd0880867003cfc3d289d516dc80bd219cd1
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-12-03 22:33:22 -0500

    finally finished create-plan function

--- src/plan/service/create_plan/interface_adapter/CreatePlanController.java
+++ src/plan/service/create_plan/interface_adapter/CreatePlanController.java
@@ -6,11 +6,11 @@ import plan.service.create_plan.CreatePlanInputBoundary;
 import user.entity.User;
 
 public class CreatePlanController {
-    final CreatePlanInputBoundary planUseCaseInteractor;
+    final CreatePlanInputBoundary createPlanUseCaseInteractor;
     public CreatePlanController(CreatePlanInputBoundary planUseCaseInteractor) {
-        this.planUseCaseInteractor = planUseCaseInteractor;
+        this.createPlanUseCaseInteractor = planUseCaseInteractor;
     }
 
     // When executed as there is no input data, will switch view to editor view only
-    public void execute(DayInfo dayInfo, Address address, User user) {planUseCaseInteractor.execute(dayInfo, address, user);}
+    public void execute(DayInfo dayInfo, Address address, User user) {createPlanUseCaseInteractor.execute(dayInfo, address, user);}
 }
