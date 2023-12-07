f66760c390af42f962d95b63342059d8e0e09cbf
minjaesong04 <minjae.song2004@gmail.com>
2023-12-07 05:19:27 -0500

    New changes

--- test/viewTest/startupViewTest.java
+++ test/viewTest/startupViewTest.java
@@ -1,8 +1,13 @@
 package viewTest;
 
+import app.LoggedInUseCaseFactory;
+import app.LoginUseCaseFactory;
+import app.SignupUseCaseFactory;
 import org.junit.Test;
 import plan.service.main_view_models.EditorViewModel;
 import plan.service.main_view_models.StartUpViewModel;
+import user.data_access.FileUserDataAccessObject;
+import user.entity.CommonUserFactory;
 import user.service.clear_users.ClearInputBoundary;
 import user.service.clear_users.interface_adapter.ClearController;
 import user.service.clear_users.interface_adapter.ClearViewModel;
@@ -11,15 +16,14 @@ import user.service.login.interface_adapter.LoginViewModel;
 import user.service.signup.SignupInputBoundary;
 import user.service.signup.interface_adapter.SignupController;
 import user.service.signup.interface_adapter.SignupViewModel;
-import view.SignupView;
-import view.StartUpView;
-import view.ViewManager;
+import view.*;
 import view.interface_adapter.ViewManagerModel;
 
 import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import static java.lang.Thread.sleep;
 
@@ -28,16 +32,9 @@ public class startupViewTest {
 
     @Test
     public void testStartUpView() {
-        // Create the controllers first
-        SignupInputBoundary sib = null;
-        SignupController signupController = new SignupController(sib);
-        ClearInputBoundary cib = null;
-        ClearController clearController = new ClearController(cib);
-
-        // Then the view manager model
+        // Essentially main in the beginning but this is a template that will be used for all views
         JFrame application = new JFrame("Planner StartUp");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
 
         CardLayout cardLayout = new CardLayout();
 
@@ -47,47 +44,41 @@ public class startupViewTest {
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(views, cardLayout, viewManagerModel);
 
-        // After that the viewmodels
         StartUpViewModel startUpViewModel = new StartUpViewModel();
-        views.add(startupView, startupView.viewName);
-
-
         LoginViewModel loginViewModel = new LoginViewModel();
         LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
         SignupViewModel signupViewModel = new SignupViewModel();
         ClearViewModel clearViewModel = new ClearViewModel();
         EditorViewModel editorViewModel = new EditorViewModel();
 
-        // Finally create the view
-        JPanel startupView = new StartUpView(viewManagerModel, startUpViewModel, loginViewModel, signupViewModel);
-        JFrame jf = new JFrame();
-        jf.setContentPane(startupView);
-        jf.pack();
-        jf.setVisible(true);
-
-
-
-
-
-
-
-
-
-
+        FileUserDataAccessObject userDataAccessObject;
         try {
-            sleep(1000);
-        } catch (InterruptedException e) {
+            userDataAccessObject = new FileUserDataAccessObject("./testusers.csv", new CommonUserFactory());
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        JPanel panel = (JPanel) startupView.getComponent(1);
-        JButton signup = (JButton) panel.getComponent(1);
+        // Finally create the view
+        StartUpView startupView = new StartUpView(viewManagerModel, startUpViewModel, loginViewModel, signupViewModel);
+        views.add(startupView, startupView.viewName);
 
-        signup.doClick();
+        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject, userDataAccessObject, clearViewModel, startUpViewModel);
+        views.add(signupView, signupView.viewName);
 
-        // pause execution for a second
+        LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel, loggedInViewModel, userDataAccessObject, startUpViewModel);
+        views.add(loginView, loginView.viewName);
+
+        LoggedInView loggedInView = LoggedInUseCaseFactory.create(viewManagerModel, loggedInViewModel, startUpViewModel, editorViewModel);
+        views.add(loggedInView, loggedInView.viewName);
+
+        viewManagerModel.setActiveView(startupView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.setSize(1000, 700);
+        application.setVisible(true);
+
         try {
-            sleep(1000);
+            sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
